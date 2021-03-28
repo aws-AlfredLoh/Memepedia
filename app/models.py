35b9a4cc0c9d38de6e23f695f65aaa482209aeb6
Alfred Loh <alfredloh@Alfreds-MacBook-Pro.local>
2021-03-28 17:17:18 +0800

    --all

--- app/models.py
+++ app/models.py
@@ -8,4 +8,17 @@ class User(db.model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))        
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
---
+++ flasky.py
@@ -0,0 +1,52 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__='roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+
+        else:
+            session['known'] = True
+
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
