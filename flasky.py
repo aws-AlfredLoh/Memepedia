433a58573777073b28c42cdd07f8db94a328c79a
Alfred <aws.jiayong0226@gmail.com>
2021-04-02 09:04:29 +0800

    flasky.py  modify.

--- flasky.py
+++ flasky.py
@@ -1,70 +1,11 @@
 import os
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
 from app import create_app, db
 from app.models import User, Role
 from flask_migrate import Migrate
-from flask_bootstrap import Bootstrap
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASK_CONFIG' or 'default'))
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
-
-class Role(db.Model):
-    __tablename__='roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-
-        else:
-            session['known'] = True
-
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('index'))
-
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
-
-
-@app.cli.command()
-def test():
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
